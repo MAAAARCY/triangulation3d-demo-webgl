f4065602b889bbc5e2a9631e13e847f21dca8b58
MAAAAARCY <masatarou.1003@icloud.com>
2025-05-18 16:28:28 +0900

    レスポンシブデザインの対応

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,17 @@ import CameraControls from './components/cameraControls'
 import SelectObject from './components/selectObject'
 
 function App() {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  
+  if (isMobile) {
+    return (
+      <div className="mobile-warning">
+        <h1>Desktop Only</h1>
+        <p>この機能はデスクトップ端末専用です。<br />パソコンからアクセスしてください。</p>
+      </div>
+    );
+  }
+
   const unityContext = useUnityContext({
     loaderUrl: "Triangulation3dDemo/Build/webGL.loader.js",
     dataUrl: "Triangulation3dDemo/Build/webGL.data",
